f082243e545257f334b13c86f3e8be64b22d21de
412 <85489519+laybacksound96@users.noreply.github.com>
2023-01-31 04:51:47 +0900

    useEffect() 배우기

--- src/Button.js
+++ src/Button.js
@@ -1,7 +1,11 @@
 import PropTypes from "prop-types";
+import { useEffect } from "react";
 import styles from "./Button.module.css";
 
 function Button({ text }) {
+  useEffect(() => {
+    console.log("Calling the api...");
+  }, []);
   return <button className={styles.btn}>{text}</button>;
 }
 
