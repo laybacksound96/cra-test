4a7de9a562020c2578526ac839c82de669ceb7f6
412 <85489519+laybacksound96@users.noreply.github.com>
2023-02-01 02:03:10 +0900

    app.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
-import Button from "./Button";
+import { useState } from "react";
 
 function App() {
+  const [value, setValue] = useState();
+  const [values, setValues] = useState([]);
+  const onChange = (event) => {
+    setValue(event.target.value);
+  };
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if (value === "") {
+      return;
+    }
+    setValues((currentArray) => [value, ...currentArray]);
+    setValue("");
+  };
+  console.log(values);
   return (
     <div>
-      <h1>Welcome</h1>
-      <Button text={"continue"} />
+      <h1>Todos</h1>
+      <form onSubmit={onSubmit}>
+        <input value={value} onChange={onChange}></input>
+        <button type="submit">Ok</button>
+      </form>
     </div>
   );
 }
